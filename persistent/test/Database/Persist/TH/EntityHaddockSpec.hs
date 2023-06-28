03742fa799189684c7e76cbe44d81691a46bb1a8
Daniel Trowbridge <blujupiter32@pm.me>
2023-06-28 16:57:03 +0100

    Implement Haddock generation tests using top-level splice
    CommentSpec is compiled with -haddock to ensure that documentation
    added using putDoc and withDecDoc is available.

--- persistent/test/Database/Persist/TH/EntityHaddockSpec.hs
+++ persistent/test/Database/Persist/TH/EntityHaddockSpec.hs
@@ -7,13 +7,29 @@ import TemplateTestImports
 
 #if MIN_VERSION_template_haskell(2,18,0)
 import Database.Persist.TH.CommentSpec (CommentModel (..))
-import Language.Haskell.TH (DocLoc (DeclDoc), getDoc, runQ)
+import Language.Haskell.TH (DocLoc (DeclDoc), getDoc)
+import Language.Haskell.TH.Lift (lift)
+
+[d|
+    commentModelDoc :: Maybe String
+    commentModelDoc = $(lift =<< getDoc (DeclDoc 'CommentModel))
+
+    commentFieldDoc :: Maybe String
+    commentFieldDoc = $(lift =<< getDoc (DeclDoc 'commentModelName))
+    |]
 
 spec :: Spec
 spec = describe "EntityHaddockSpec" $ do
     it "generates entity Haddock" $ do
-        maybeDoc <- runQ $ getDoc (DeclDoc 'CommentModel)
-        maybeDoc `shouldBe` Just "Doc comments work.\nHas multiple lines."
+        let expected = unlines [ "Doc comments work."
+                               , "Has multiple lines."
+                               ]
+        commentModelDoc `shouldBe` Just expected
+    it "generates field Haddock" $ do
+        let expected = unlines [ "First line of comment on column."
+                               , "Second line of comment on column."
+                               ]
+        commentFieldDoc `shouldBe` Just expected
 #else
 spec :: Spec
 spec = pure ()
