ae317ead14b68149b9d0bb973513e5b91d71ba6c
Daniel Trowbridge <blujupiter32@pm.me>
2023-06-28 01:26:30 +0100

    Only run failing spec on GHC versions with TH Haddock support

--- persistent/test/Database/Persist/TH/EntityHaddockSpec.hs
+++ persistent/test/Database/Persist/TH/EntityHaddockSpec.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Database.Persist.TH.EntityHaddockSpec (spec) where
 
 import TemplateTestImports
 
+#if MIN_VERSION_template_haskell(2,18,0)
 import Database.Persist.TH.CommentSpec (CommentModel (..))
 import Language.Haskell.TH (DocLoc (DeclDoc), getDoc, runQ)
 
@@ -12,3 +14,7 @@ spec = describe "EntityHaddockSpec" $ do
     it "generates entity Haddock" $ do
         maybeDoc <- runQ $ getDoc (DeclDoc 'CommentModel)
         maybeDoc `shouldBe` Just "Doc comments work.\nHas multiple lines."
+#else
+spec :: Spec
+spec = pure ()
+#endif
